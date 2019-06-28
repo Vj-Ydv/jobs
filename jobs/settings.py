fd3d3c703b5f771d5bc2925f259df049550cdfca
vijay yadav <vizayyadav123@gmail.com>
2019-06-28 08:47:42 +0545

    commit

--- jobs/settings.py
+++ jobs/settings.py
@@ -108,6 +108,9 @@ DATABASES = {
         
     }
 }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(default='postgres://USER:PASSWORD@HOST:PORT/NAME')
+DATABASES['default'] = dj_database_url.parse('postgres://USER:PASSWORD@HOST:PORT/NAME', conn_max_age=600)
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
